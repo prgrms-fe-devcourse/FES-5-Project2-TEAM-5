b56f59af59ac103ed163043f8a467b1c2124acb1
Jaden <hyunbin.kim.3376@gmail.com>
2025-07-22 09:16:37 +0900

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import { flatConfigsToPlugins } from 'eslint-plugin-prettier';
+import { globalIgnores } from 'eslint/config';
+
+export default tseslint.config([
+  globalIgnores(['dist', 'node_modules']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off', // React 17+
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  react.configs.recommended,
+  reactHooks.configs['recommended'],
+  reactRefresh.configs.vite,
+]);
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/global.css';
+
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Toaster /> {/* react-hot-toast */}
+      </div>
+    </>
+  );
+}
+
+export default App;
