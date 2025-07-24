7f90cd4c033218e83820eb0b89c2ea8c73d3b4d3
yoon00 <y00ndlek@gmail.com>
2025-07-24 12:30:55 +0900

    style: community diary select ui 변경

---
+++ src/pages/feed-diaries/diaryList/components/EmotionSelectBox/customStyle.ts
@@ -0,0 +1,62 @@
+import type { Emotion } from '../..';
+import { type StylesConfig } from 'react-select';
+
+export const customStyles: StylesConfig<Emotion, true> = {
+  control: (provided) => ({
+    ...provided,
+    width: '100px',
+    height: '28px',
+    borderRadius: '8px',
+    background: '#6B8A47',
+    color: 'white',
+  }),
+  option: (provided) => ({
+    ...provided,
+    backgroundColor: '#A7C584',
+    color: 'white',
+    fontSize: '16px',
+  }),
+  menu: (provided) => ({
+    ...provided,
+    width: '100px',
+    backgroundColor: '#A7C584',
+  }),
+  multiValue: (provided) => ({
+    ...provided,
+    backgroundColor: 'transparent',
+  }),
+  multiValueLabel: (provided) => ({
+    ...provided,
+    color: 'white',
+  }),
+  input: (provided) => ({
+    ...provided,
+    color: 'white',
+  }),
+  dropdownIndicator: (provided) => ({
+    ...provided,
+    color: 'white',
+  }),
+  noOptionsMessage: (provided) => ({
+    ...provided,
+    color: 'white',
+  }),
+  clearIndicator: (provided) => ({
+    ...provided,
+    color: 'white',
+  }),
+  placeholder: (provided) => ({
+    ...provided,
+    color: 'white',
+  }),
+  indicatorSeparator: (provided) => ({
+    ...provided,
+    display: 'none',
+  }),
+  valueContainer: (provided) => ({
+    ...provided,
+    flexWrap: 'nowrap',
+    overflowX: 'auto',
+    maxWidth: '100%',
+  }),
+};
