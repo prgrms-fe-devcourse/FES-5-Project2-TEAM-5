9af0404b1b28862b692538451c15d543cb97ef40
Jaden <hyunbin.kim.3376@gmail.com>
2025-07-25 23:40:23 +0900

    feat: useUser 공통 함수 users 테이블 데이터 추가

--- src/shared/hooks/useUser.ts
+++ src/shared/hooks/useUser.ts
@@ -1,33 +1,50 @@
 import { useEffect, useState } from 'react';
 import supabase from '../supabase/supabase';
 import type { User } from '@supabase/supabase-js';
-import { getUserProfile } from '../utils/supabase/getUserProfile';
+import type { Tables } from '../supabase/database.types';
 
 export const useUser = () => {
   const [user, setUser] = useState<User | null>(null);
-  const [profileImage, setProfileImage] = useState<string | null>(null);
+  const [userInfo, setUserInfo] = useState<Tables<'users'> | null>(null);
 
   useEffect(() => {
-    supabase.auth.getUser().then(({ data }) => {
-      setUser(data.user);
-    });
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_, session) => {
+    const initializeUser = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
       setUser(session?.user ?? null);
-      if (!session?.user) {
-        setProfileImage(null);
-      }
-    });
 
-    return () => subscription.unsubscribe();
+      const {
+        data: { subscription },
+      } = supabase.auth.onAuthStateChange(async (_, session) => {
+        setUser(session?.user ?? null);
+
+        if (!session?.user) {
+          setUserInfo(null);
+        }
+      });
+      return () => subscription.unsubscribe();
+    };
+
+    initializeUser();
   }, []);
 
   useEffect(() => {
-    if (user) {
-      getUserProfile(user.id).then(setProfileImage);
+    if (!user) {
+      setUserInfo(null);
+      return;
     }
+
+    const getUserData = async () => {
+      const { data, error } = await supabase.from('users').select().eq('id', user.id).single();
+      if (error) {
+        console.error(`사용자 정보 로드 실패 : ${error}`);
+        setUserInfo(null);
+      }
+      setUserInfo(data);
+    };
+
+    getUserData();
   }, [user]);
 
   const logout = async (): Promise<void> => {
@@ -37,5 +54,5 @@ export const useUser = () => {
     }
   };
 
-  return { user, isAuth: !!user, logout, profileImage };
+  return { user, userInfo, isAuth: !!user, logout, profileImage: userInfo?.profile_image };
 };
