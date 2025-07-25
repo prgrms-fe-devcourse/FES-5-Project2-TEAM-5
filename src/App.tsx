8f5359985f426bf1f39fc77877453d2da4879aa0
skyunlee98 <skyunlee@naver.com>
2025-07-25 18:41:40 +0900

    refactor: App.tsx에서 테스트용 임시 라우트 제거

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,15 @@ import './styles/global.css';
 import { Toaster } from 'react-hot-toast';
 
 // 감정분석 페이지
-import SelectDiary from './pages/analysis/SelectDiary/index';
-import EmotionAndQuest from './pages/analysis/EmotionAndQuest/index';
-import SelectQuest from './pages/analysis/SelectQuest/index';
+import SelectDiary from './pages/analysis/SelectDiary';
+import EmotionAndQuest from './pages/analysis/EmotionAndQuest';
+import SelectQuest from './pages/analysis/SelectQuest';
 
 function App() {
   return (
     <BrowserRouter>
       <Toaster /> {/* react-hot-toast */}
       <Routes>
-        {/* 테스트용: / 경로에서 세 가지 페이지 모두 렌더링 */}
-        <Route
-          path="/"
-          element={
-            <div>
-              {/* <SelectDiary /> */}
-              <EmotionAndQuest />
-              {/* <SelectQuest /> */}
-            </div>
-          }
-        />
-
-        {/* 감정분석 플로우 */}
         <Route path="/analysis/select-diary" element={<SelectDiary />} />
         <Route path="/analysis/emotion-and-quest" element={<EmotionAndQuest />} />
         <Route path="/analysis/quest-select" element={<SelectQuest />} />
