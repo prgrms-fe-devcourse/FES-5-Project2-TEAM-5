130e042f908d4451c3616b183bda5f80ee66b893
Jaden <hyunbin.kim.3376@gmail.com>
2025-07-25 23:41:42 +0900

    feat: UserInfoSection 유저 정보 api 연결

--- src/pages/Mypage/components/UserInfoSection/index.tsx
+++ src/pages/Mypage/components/UserInfoSection/index.tsx
@@ -1,18 +1,26 @@
 import S from './style.module.css';
 import defaultProfile from '../../assets/defaultProfile.svg';
+import { useUser } from '@/shared/hooks/useUser';
+import { formatToReadableDate } from '../../utils/helper';
 
 const UserInfoSection = () => {
+  const { userInfo } = useUser();
+
   return (
     <section className={S.hero}>
       <h2 className="sr-only">사용자 정보</h2>
       <div className={S.profileSection}>
-        <img src={`${defaultProfile}`} alt="기본 프로필" />
+        <img
+          src={userInfo?.profile_image ? userInfo.profile_image : defaultProfile}
+          alt="사용자 프로필"
+        />
       </div>
       <div className={S.info}>
-        <span className={S.username}>Sophia</span>
+        <span className={S.username}>{userInfo?.name}</span>
         <span>Sophia.miller@email.com</span>
         <span>
-          Joined on Jan 15, 2023 | Today's mood: <strong className={S.userMood}>Happy</strong>
+          Joined on {formatToReadableDate(userInfo?.created_at || '')} | Today's mood:{' '}
+          <strong className={S.userMood}>Happy</strong>
         </span>
       </div>
     </section>
