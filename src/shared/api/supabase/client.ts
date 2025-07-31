e91cde3848511eef97c61d1dfc0932596d01c4fc
Jaden <hyunbin.kim.3376@gmail.com>
2025-07-31 11:32:04 +0900

    feat: 타입 업데이트

--- src/shared/api/supabase/client.ts
+++ src/shared/api/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-// npx supabase gen types typescript --project-id ttqedeydfvolnyrivpvg > src/shared/supabase/types.ts
+// npx supabase gen types typescript --project-id ttqedeydfvolnyrivpvg > src/shared/api/supabase/types.ts
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
