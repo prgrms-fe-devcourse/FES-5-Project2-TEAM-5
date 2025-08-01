bba15a22d7ee67c969d9ad43d5f5e98a86dc8c6e
Jaden <hyunbin.kim.3376@gmail.com>
2025-08-01 22:49:30 +0900

    style: diaryRowCard ui 수정

--- src/shared/components/DiaryRowCard/index.tsx
+++ src/shared/components/DiaryRowCard/index.tsx
@@ -16,41 +16,43 @@ function DiaryRowCard({
 }: DiaryRowEntity) {
   const navigate = useNavigate();
 
-  const handleClick = (e: React.MouseEvent<HTMLLIElement>) => {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     navigate(`/diary/${id}`);
   };
 
   return (
-    <li className={S.rowCard} onClick={handleClick}>
+    <li className={S.rowCard}>
       {is_analyzed && <div className={S.overlay} />}
-      <div className={S.diaryContents}>
-        <span>
-          {emotion_mains.icon_url && (
-            <img src={emotion_mains.icon_url || ''} alt={emotion_mains.name || '감정'} />
-          )}
+      <a href={`/diary/${id}`} className={S.container} onClick={handleClick}>
+        <div className={S.diaryContents}>
+          <span>
+            {emotion_mains.icon_url && (
+              <img src={emotion_mains.icon_url || ''} alt={emotion_mains.name || '감정'} />
+            )}
 
-          {emotion_mains.name}
-        </span>
-        <p className={S.title}>{title}</p>
-        <ul className={S.tagList}>
-          {diary_hashtags.map((tag) => (
-            <li key={tag.id}>#{tag.name}</li>
-          ))}
-        </ul>
-        <div className={S.empathy}>
-          <span>{likes} Likes</span> · <span>{comments} Comments</span>
+            {emotion_mains.name}
+          </span>
+          <p className={S.title}>{title}</p>
+          <ul className={S.tagList}>
+            {diary_hashtags.map((tag) => (
+              <li key={tag.id}>#{tag.name}</li>
+            ))}
+          </ul>
+          <div className={S.empathy}>
+            <span>{likes} Likes</span> · <span>{comments} Comments</span>
+          </div>
+          <p className={S.date}>{formatToReadableDate(created_at)}</p>
         </div>
-        <p className={S.date}>{formatToReadableDate(created_at)}</p>
-      </div>
-      <figure>
-        {diary_image && (
-          <>
-            <img className={S.thumbImage} src={diary_image} alt="" />
-            <figcaption className="sr-only">일기 첨부 사진</figcaption>
-          </>
-        )}
-      </figure>
+        <figure>
+          {diary_image && (
+            <>
+              <img className={S.thumbImage} src={diary_image} alt="" />
+              <figcaption className="sr-only">일기 첨부 사진</figcaption>
+            </>
+          )}
+        </figure>
+      </a>
     </li>
   );
 }
